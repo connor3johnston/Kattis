b66ca111e506bf90c9850149b925870456c88787
Connor Johnston <connor3johnston@gmail.com>
2019-02-12 00:48:06 -0600

    solved detaileddifferences

---
+++ PYTHON/detaileddifferences/detaileddifferences.py
@@ -0,0 +1,21 @@
+# Rating: ~ 1.4 / 10
+# Link: https://open.kattis.com/problems/detaileddifferences
+
+def main():
+  cases = int(input())
+  for x in range(cases):
+    line1 = input()
+    line2 = input()
+    build = ''
+    for index, letter in enumerate(line1):
+      if letter == line2[index]:
+        build += '.'
+      else:
+        build += '*'
+    print(line1)
+    print(line2)
+    print(build)
+    print()
+
+if __name__ == "__main__":
+  main()
