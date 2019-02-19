f0e1b8e095b5d3901ae4e6677c912e426f53f962
Connor Johnston <connor3johnston@gmail.com>
2019-02-19 00:08:43 -0600

    solved flyingsafely

---
+++ PYTHON/flyingsafely/flyingsafely.py
@@ -0,0 +1,13 @@
+# Rating: ~ 1.6 / 10
+# Link: https://open.kattis.com/problems/flyingsafely
+
+def main():
+  cases = int(input())
+  for _ in range(cases):
+    cities, pilots = map(int, input().split())
+    for _ in range(pilots):
+      a,b = input().split()
+    print(cities-1)
+
+if __name__ == "__main__":
+  main()
