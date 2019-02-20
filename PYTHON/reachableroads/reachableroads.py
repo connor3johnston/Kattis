a89c0dc085d2dea9361a4e831129218fb6a74ee8
Connor Johnston <connor3johnston@gmail.com>
2019-02-19 21:39:28 -0600

    solved reachableroads

---
+++ PYTHON/reachableroads/reachableroads.py
@@ -0,0 +1,32 @@
+# Rating: ~ 2.5 / 10
+# Link: https://open.kattis.com/problems/reachableroads
+
+def union(track: list, l: int, r: int):
+  parL = find(track, l)
+  parR = find(track, r)
+  track[parR] = parL
+
+
+def find(track: list, x: int):
+  if track[x] == x:
+    return x
+  track[x] = find(track, track[x])
+  return track[x]
+
+
+def main():
+  cases = int(input())
+  for x in range(cases):
+    roads = int(input())
+    track = list(range(0,roads))
+    ends = int(input())
+    for y in range(ends):
+      line = [int(p) for p in input().split()]
+      union(track, line[0], line[1])
+    print_ = set()
+    for _ in range(roads):
+      print_.add(find(track, _))
+    print(len(print_)-1)
+
+if __name__ == "__main__":
+  main()
