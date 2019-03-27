0fe186b02faf9da37934812d469f2eac1f7fb3b2
Connor Johnston <connor3johnston@gmail.com>
2019-03-27 12:06:22 -0500

    solved hissingmicrophone

---
+++ PYTHON/hissingmicrophone/hissingmicrophone.py
@@ -0,0 +1,12 @@
+# Rating: ~ 1.2 / 10
+# Link: https://open.kattis.com/problems/hissingmicrophone
+
+def main():
+  if 'ss' in input():
+    print('hiss')
+  else:
+    print('no hiss')
+
+
+if __name__ == "__main__":
+  main()
