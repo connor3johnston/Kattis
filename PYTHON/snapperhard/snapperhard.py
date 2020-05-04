0563cff1d8e9d35a7197f5b10ab4cb14a4204e6f
Connor Johnston <connor3johnston@gmail.com>
2020-05-04 13:52:10 -0500

    Solved snapperhard

---
+++ PYTHON/snapperhard/snapperhard.py
@@ -0,0 +1,20 @@
+from sys import stdin, stdout
+
+# Rating: ~ 2.3 / 10
+# Link: https://open.kattis.com/problems/snapperhard
+
+def main():
+  cases = int(stdin.readline())
+  for i in range(cases):
+    snappers, snaps = map(int, stdin.readline().split())
+    base = 2 ** snappers
+    state = 'OFF'
+
+    if (snaps + 1) % base == 0:
+      state = 'ON'
+
+    stdout.write(f'Case #{i + 1}: {state}\n')
+
+
+if __name__ == "__main__":
+  main()
